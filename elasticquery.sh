d48b1476a31821e9fec4cbfe6de953fda4599dc0
Frank Lyaruu <frank@dexels.com>
2018-11-15 17:07:10 +0100

    update

--- elasticquery.sh
+++ elasticquery.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl http://replication-demo.dexels.com:9200/dvdstore-test-generation-14-simple-dvdstore-test-generation-14-simple-dvd/_search?q=Gina+Monkey\&pretty=true\&_source=title| jq .hits.hits[]._source.title
+curl http://replication-demo.dexels.com:9200/dvdstore-test-generation-3-simple-dvdstore-test-generation-3-simple-film/_search?q=Gina+Darkness\&_source=title | jq .hits.hits[]._source.title
